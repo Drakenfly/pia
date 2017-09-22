3a8f96017c860393a5452cf55e8f0e90d898e6d5
mbrexner <mbrexner@devsrv.centec.at>
2017-09-22 14:29:41 +0200

    Add initial java classes

---
+++ cli/src/main/java/com/pia/cli/Application.java
@@ -0,0 +1,10 @@
+package com.pia.cli;
+
+import com.pia.core.Generator;
+
+public class Application {
+    public static void main(String[] args) {
+        Generator generator = new Generator();
+        generator.start();
+    }
+}
---
+++ core/src/main/java/com/pia/core/Generator.java
@@ -0,0 +1,7 @@
+package com.pia.core;
+
+public class Generator {
+    public void start() {
+        System.out.println("Generator has been started");
+    }
+}
