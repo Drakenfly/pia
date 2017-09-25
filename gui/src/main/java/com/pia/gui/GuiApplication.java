816c68f60d09093f7c297cb037595c5acab9d3f8
Markus <indiemarkus@gmail.com>
2017-09-25 21:04:55 +0200

    Add gui module to project

---
+++ gui/src/main/java/com/pia/gui/GuiApplication.java
@@ -0,0 +1,11 @@
+package com.pia.gui;
+
+import com.pia.core.Generator;
+
+public class GuiApplication {
+    public static void main(String[] args) {
+        //TODO add javafx gui
+        Generator generator = new Generator();
+        generator.start();
+    }
+}
