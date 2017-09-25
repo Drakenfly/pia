07e616217dc35a988fa0ae8327f7b814de1b6680
Markus <indiemarkus@gmail.com>
2017-09-25 21:50:27 +0200

    Make Gui Loadable

--- gui/src/main/java/com/pia/gui/GuiApplication.java
+++ gui/src/main/java/com/pia/gui/GuiApplication.java
@@ -2,10 +2,25 @@ package com.pia.gui;
 
 import com.pia.core.Generator;
 
-public class GuiApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GuiApplication extends Application{
     public static void main(String[] args) {
-        //TODO add javafx gui
+        launch(args);
         Generator generator = new Generator();
         generator.start();
+    }
+
+    @Override
+    public void start (Stage primaryStage) throws Exception {
+        //Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 }
---
+++ gui/src/main/java/com/pia/gui/controllers/Controller.java
@@ -0,0 +1,4 @@
+package com.pia.gui.controllers;
+
+public class Controller {
+}
