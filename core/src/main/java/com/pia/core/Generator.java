2a10febf3af52999fba66f71251dd938c0f4219a
Markus <indiemarkus@gmail.com>
2017-09-25 20:54:07 +0200

    Remove unneccessary try catch block

--- core/src/main/java/com/pia/core/Generator.java
+++ core/src/main/java/com/pia/core/Generator.java
@@ -12,29 +12,25 @@ public class Generator {
     PluginService pluginService = new PluginService();
 
     public void start() {
-        try {
-            File pluginsFolder = new File("plugins");
-            List<File> jarFiles = Arrays.asList(pluginsFolder.listFiles(file -> file.getPath().toLowerCase().endsWith(".jar")));
-            List<URL> jarUrls = new ArrayList<>();
-            jarFiles.forEach(file -> {
-                try {
-                    jarUrls.add(file.toURI().toURL());
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-            });
-            URLClassLoader urlCl = new URLClassLoader(jarUrls.toArray(new URL[0]));
-
-            System.out.println("Printing plugin-list:");
-            ServiceLoader<PiaPlugin> serviceLoader = ServiceLoader.load(PiaPlugin.class, urlCl);
-            for (PiaPlugin aServiceLoader : serviceLoader) {
-                pluginService.addPlugin(aServiceLoader);
-                System.out.println(aServiceLoader.getName());
+        File pluginsFolder = new File("plugins");
+        List<File> jarFiles = Arrays.asList(pluginsFolder.listFiles(file -> file.getPath().toLowerCase().endsWith(".jar")));
+        List<URL> jarUrls = new ArrayList<>();
+        jarFiles.forEach(file -> {
+            try {
+                jarUrls.add(file.toURI().toURL());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
             }
-            System.out.println("Finished printing plugin-list");
-        } catch (Exception ex) {
+        });
+        URLClassLoader urlCl = new URLClassLoader(jarUrls.toArray(new URL[0]));
 
+        System.out.println("Printing plugin-list:");
+        ServiceLoader<PiaPlugin> serviceLoader = ServiceLoader.load(PiaPlugin.class, urlCl);
+        for (PiaPlugin aServiceLoader : serviceLoader) {
+            pluginService.addPlugin(aServiceLoader);
+            System.out.println(aServiceLoader.getName());
         }
+        System.out.println("Finished printing plugin-list");
 
         pluginService.resolveRequirements();
         pluginService.start();
